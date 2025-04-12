90920cde5eea9bc7078f280243e6bd5160ba937c
DimaTurkey <123176664+DimaTurkey@users.noreply.github.com>
2025-04-12 21:10:01 +0300

    new-code

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Привіт! Це ваш перший проєкт на Node.js!");
+const test1 = {
+    user: 'Dima'
+}
+const test2 = ['dima', 'summer', 'friends', 11, "turkeys"]
+console.log(test1)
+console.log(test2)
